488f11492134ad79bc9b629a29f14fd2a635c7be
steelbrain <aneesiqbalbhatti@gmail.com>
2015-08-22 15:03:54 -0600

    :arrow_up: Bump version to 1.0.0

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,77 @@
-"use strict";function _classCallCheck(e,n){if(!(e instanceof n))throw new TypeError("Cannot call a class as a function")}var _createClass=function(){function e(e,n){for(var t=0;t<n.length;t++){var s=n[t];s.enumerable=s.enumerable||!1,s.configurable=!0,"value"in s&&(s.writable=!0),Object.defineProperty(e,s.key,s)}}return function(n,t,s){return t&&e(n.prototype,t),s&&e(n,s),n}}(),EventEmitter=require("zm-event-kit").Emitter,Communication=function(){function e(n,t){_classCallCheck(this,e),this.send=n,this.debug=t}return _createClass(e,[{key:"gotMessage",value:function(e){var n=this;if(e.SB)if(Debug&&console.debug(e),"send"===e.Genre){e.response=null;var t=void 0;try{this.emit(e.Type,e),t=e.response instanceof Promise?e.response:Promise.resolve(e.response)}catch(s){t=Promise.reject(s)}t.then(function(t){n.send({Genre:"response",Status:!0,Result:t,ID:e.ID,SB:!0})},function(t){t instanceof Error&&(t={stack:t.stack,message:t.message}),n.send({Genre:"response",Status:!1,Result:t,ID:e.ID,SB:!0})})}else"response"===e.Genre&&this.emit("JOB:"+e.ID,e)}},{key:"request",value:function(n,t){var s=this;return new Promise(function(r,o){var i=e.randomId(),a=s.on("JOB:"+i,function(e){a.dispose(),e.Status?r(e.Result):o(e.Result)});s.send({Type:n,Genre:"send",Message:t,SB:!0,ID:i})})}}],[{key:"randomId",value:function(){return(Math.random().toString(36)+"00000000000000000").slice(2,9)}}]),e}();module.exports=Communication;
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var EventEmitter = require('zm-event-kit').Emitter;
+
+var Communication = (function (_EventEmitter) {
+  _inherits(Communication, _EventEmitter);
+
+  function Communication(sendCallback, debug) {
+    _classCallCheck(this, Communication);
+
+    _get(Object.getPrototypeOf(Communication.prototype), 'constructor', this).call(this);
+    this.send = sendCallback;
+    this.debug = debug;
+  }
+
+  _createClass(Communication, [{
+    key: 'gotMessage',
+    value: function gotMessage(message) {
+      var _this = this;
+
+      if (!message.SB) return;
+      if (this.debug) console.debug(message);
+
+      if (message.Genre === 'send') {
+        message.response = null;
+        var response = undefined;
+        try {
+          this.emit(message.Type, message);
+          response = message.response instanceof Promise ? message.response : Promise.resolve(message.response);
+        } catch (err) {
+          response = Promise.reject(err);
+        }
+        response.then(function (retVal) {
+          _this.send({ Genre: 'response', Status: true, Result: retVal, ID: message.ID, SB: true });
+        }, function (retVal) {
+          if (retVal instanceof Error) {
+            retVal = { stack: retVal.stack, message: retVal.message };
+          }
+          _this.send({ Genre: 'response', Status: false, Result: retVal, ID: message.ID, SB: true });
+        });
+      } else if (message.Genre === 'response') {
+        this.emit('JOB:' + message.ID, message);
+      }
+    }
+  }, {
+    key: 'request',
+    value: function request(type, message) {
+      var _this2 = this;
+
+      return new Promise(function (resolve, reject) {
+        var JobID = Communication.randomId();
+        var disposable = _this2.on('JOB:' + JobID, function (Message) {
+          disposable.dispose();
+          if (Message.Status) resolve(Message.Result);else reject(Message.Result);
+        });
+        _this2.send({ Type: type, Genre: 'send', Message: message, SB: true, ID: JobID });
+      });
+    }
+  }], [{
+    key: 'randomId',
+    value: function randomId() {
+      return (Math.random().toString(36) + '00000000000000000').slice(2, 7 + 2);
+    }
+  }]);
+
+  return Communication;
+})(EventEmitter);
+
+module.exports = Communication;
